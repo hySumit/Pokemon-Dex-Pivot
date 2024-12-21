cea6d8732c8d83c2061a9e996902b9ca31a9aa69
Sumit Kumar <sumitbth67@gmail.com>
2024-12-21 21:38:04 +0530

    added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { PokemonProvider } from "./components/context/PokemonContext";
 import PokedexGrid from "./components/PokedexGrid";
 import PokemonTypeFilter from "./components/PokemonTypeFilter";
+import { FaGithub } from "react-icons/fa";
 
 const App = () => {
   const [selectedTypes, setSelectedTypes] = useState([]);
@@ -9,9 +10,24 @@ const App = () => {
   return (
     <PokemonProvider>
       <div className="min-h-screen bg-gray-100 p-4">
-        <h1 className="text-2xl font-bold mb-4">Pokémon Explorer</h1>
+        <h1 className="text-2xl font-bold mb-4 flex items-center">
+          Pokémon Explorer- Sumit Kumar
+          <span className="m-1">
+            <a href="https://github.com/hySumit">
+              <FaGithub />
+            </a>
+          </span>
+        </h1>
         <PokemonTypeFilter
-          availableTypes={["grass", "fire", "water", "electric", "rock", "normal", "ghost",]}
+          availableTypes={[
+            "grass",
+            "fire",
+            "water",
+            "electric",
+            "rock",
+            "normal",
+            "ghost",
+          ]}
           selectedTypes={selectedTypes}
           onTypeSelect={setSelectedTypes}
         />
